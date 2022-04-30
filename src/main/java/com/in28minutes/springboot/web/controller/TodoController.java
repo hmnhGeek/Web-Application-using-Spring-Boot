9fa51e48fe6a38c605f998f19eff7cf739a7a246
himanshu_sharma15 <himanshu_sharma15@dell.com>
2022-04-30 15:21:17 +0530

    .

--- src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.web.controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -9,11 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.in28minutes.springboot.web.model.Todo;
 import com.in28minutes.springboot.web.service.LoginService;
 import com.in28minutes.springboot.web.service.TodoService;
 
 @Controller
-@SessionAttributes({"display_name"})
+@SessionAttributes("display_name")
 public class TodoController {
 	
 	@Autowired
@@ -22,8 +25,27 @@ public class TodoController {
 	@RequestMapping(value="/list-todos", method=RequestMethod.GET)
 	public String showTodos(ModelMap model) {
 		String name = (String)model.get("display_name");
-		System.out.println(name);
 		model.put("todos", _todoService.retrieveTodos(name));
 		return "list-todos";
+	}
+	
+	@RequestMapping(value="/add-todo", method=RequestMethod.GET)
+	public String showAddTodoPage(ModelMap model) {
+		// add a dummy todo here whose values will be modified in the form.
+		model.addAttribute("todo", new Todo(0, (String) model.get("name"), "Default Desc",
+				new Date(), false));
+		return "todo";
+	}
+	
+	@RequestMapping(value="/delete-todo", method=RequestMethod.GET)
+	public String deleteTodo(@RequestParam int id) {
+		_todoService.deleteTodo(id);
+		return "redirect:/list-todos";
+	}
+	
+	@RequestMapping(value="/add-todo", method=RequestMethod.POST)
+	public String addTodo(ModelMap model, @RequestParam String desc) {
+		_todoService.addTodo((String)model.get("display_name"), desc, new Date(), false);
+		return "redirect:/list-todos";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/list-todos.jsp
+++ src/main/webapp/WEB-INF/jsp/list-todos.jsp
@@ -1,8 +1,39 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <html>
 <head>
-	<title>First web application</title>
+	<title>Todos for ${display_name}</title>
+	<link href="webjars/bootstrap/3.3.6/css/bootstrap.min.css"
+	    		rel="stylesheet">
 </head>
 <body>
-	Here are your todos: ${todos}
+	Here are your todos:
+	<div class="container">
+		<table class="table striped">
+			<thead>
+				<tr>
+					<th>Description</th>
+					<th>Target date</th>
+					<th>Is it done?</th>
+					<th>Delete</th>
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach items="${todos}" var="todo">
+					<tr>
+						<td>${todo.desc}</td>
+						<td>${todo.targetDate}</td>
+						<td>${todo.done}</td>
+						<td><a href="/delete-todo?id=${todo.id}" type="button" class="btn btn-warning">Delete</a></td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+		<br />
+		Add todos <div><a class="button" href="/add-todo">here</a></div>
+	</div>
+	
+	<script src="webjars/jquery/1.9.1/jquery.min.js"></script>
+	<script src="webjars/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
